0ad8d6dabfbfdba97713a6f3e307cd98b69e9100
joseph lee <meliora000@github.com>
2016-07-27 02:09:28 +0900

    first commit

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,24 @@
+class CategoriesController < ApplicationController
+  before_action :set_category, only: [:show]
+  def index
+    @categories = Category.all
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @categories}
+    end
+  end
+
+  def show
+    @posts = @category.posts.order("created_at DESC")
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: [@category,@posts]}
+    end
+  end
+
+  private
+  def set_category
+    @category = Category.find(params[:id])
+  end
+end
